3b3b7dd55b5f8c1785a98f1d3183d211af08b993
TsuiWide <xukuan89@163.com>
2023-05-17 22:05:29 +0800

    add neo tree

---
+++ config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "hrsh7th/nvim-cmp",
+        event = { "InsertEnter", "CmdlineEnter" },
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "saadparwaiz1/cmp_luasnip",
+        },
+    }
+}
