ed191c02b215f4d52114123acf380e8efd51a2d8
TsuiWide <xukuan89@163.com>
2023-06-01 20:12:55 +0800

    add options and keymaps

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,38 +1,4 @@
-vim.g.mapleader = " "
+require "options"
+require "keymaps"
 
-local set = vim.o
 
-set.number = true
-set.relativenumber = true
-set.clipboard = "unnamed"
-
-require("autocmd")
-require("keymaps")
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    im.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
-end
-
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins", {
-})
-
---vim.cmd.colorscheme("base16-tender")
---vim.cmd.colorscheme("tokyonight")
-vim.cmd.colorscheme("gruvbox")
-
-local lspconfig = require('lspconfig')
-
-require("mason").setup()
-require("mason-lspconfig").setup()
-
-require("lspconfig").lua_ls.setup {}
